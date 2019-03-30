e271f05c58a91263a7e75b4bca1cee0e576d007f
Atia Mary Amoah <atiamary14@gmail.com>
2019-03-30 14:01:32 +0000

    my djangogirls app first commit

---
+++ play/hello.py
@@ -0,0 +1,15 @@
+volume=110
+if volume<20:
+	print('its kinda quiet')
+elif 20<= volume <40:
+	print('it\'s nice for background music')
+elif 40<= volume<60:
+	print ('perfect i can hear all the details')
+elif 60 <= volume <80:
+	print ("nice for parties")
+elif 80<=volume<100:
+	print('a bit loud')
+else:
+	print('my ears are hurting')
+
+#this is my own code
